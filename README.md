4fc4bdabfd6b5e52c1f21aed7343c99f983c3617
manny-uncharted <omoebun52@gmail.com>
2023-02-11 13:49:33 +0100

    adding the files

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# Face-Recognition
+# Face-Recognition\
+
+
+The goal of the project is to create a facial recognition system that.
+
+It contains a PCA algorithm to reduce the dimensionality of the data and LDA algorithm to classify the data.
+It also has a CNN to classify the data and recognize if the person is in the database or not.
--- pca.py
+++ pca.py
@@ -1,22 +1,24 @@
 import sys
-from sklearn.decomposition import RandomizedPCA
+# import sklearn
+from sklearn.decomposition import PCA
 import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
 import numpy as np
 
-filename = []
+filename = ['A1.jpg', 'A2.jpg', 'A3.jpg', 'B1.jpg', 'B2.jpg', 'B3.jpg', 'C1.jpg', 'C2.jpg', 'C3.jpg', 'D1.jpg', 'D2.jpg', 'D3.jpg', 'E1.jpg', 'E2.jpg', 'E3.jpg', 'F1.jpg', 'F2.jpg', 'F3.jpg',]
 
-for i in range(1, len(sys.argv)):
-    img = mpimg.imread('input/' + sys.argv[i])
+
+for i in range(1, len(filename)):
+    img = mpimg.imread('training_set/' + filename[i])
     print(img.shape)
     plt.axis('off')
     plt.imshow(img)
 
     # reshape the image
-    img_r = np.reshape(img, (img.shape[0], img.shape[1] * img.shape[2]))
+    img_r = np.reshape(img, (img.shape[0], img.shape[1]* img.shape[2]))
     print(img_r.shape)
 
-    ipca = RandomizedPCA(int(sys.argv[1]).fit(img_r))
+    ipca = PCA(20).fit(img_r)
     img_c = ipca.transform(img_r)
     print(img_c.shape)
     print(np.sum(ipca.explained_variance_ratio_))
